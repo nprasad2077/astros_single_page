ab73b13c423c21f7e2d9ffddcdd8f359de9e1165
Nikhil Prasad <nprasad2077@gmail.com>
2023-09-29 03:20:58 -0500

    data flows from call to table

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+// Components
+import SinglePitch from "./components/SinglePitch";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getAstrosData = async () => {
+      try {
+        const response = await axios.get(
+          "https://raw.githubusercontent.com/rd-astros/hiring-resources/master/pitches.json"
+        );
+        console.log(response.data.queryResults.row);
+        setData(response.data.queryResults.row);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAstrosData();
+  }, []);
+
+  console.log(data[0]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <SinglePitch pitch={data[0]} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/SinglePitch.jsx
+++ src/components/SinglePitch.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
+import "../styles/SinglePitch.css";
 
-const SinglePitch = () => {
+const SinglePitch = ({ pitch }) => {
+
+  console.log((pitch));
+
   return (
-    <div>SinglePitch</div>
-  )
-}
+    <div>
+      <table className="pitch-table">
+        <thead>
+          <tr>
+            <th colSpan={2}>Pitch Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Strike Zone (Top)
+            </td>
+            <td>
+              {pitch.sz_top}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default SinglePitch
+export default SinglePitch;
