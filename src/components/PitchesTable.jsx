aac4e918240b4bc4e0ba5a9606fbcdc1c6f69f1c
Nikhil Prasad <nprasad2077@gmail.com>
2023-09-29 22:17:59 -0500

    color update

--- src/components/PitchesTable.jsx
+++ src/components/PitchesTable.jsx
@@ -6,18 +6,26 @@ const PitchesTable = ({ data, onSelectPitch }) => {
     <table className="pitches-table">
       <thead>
         <tr>
-            <th>Inning & Count</th>
-            <th>Pitch Type & Speed</th>
-            <th>Batter vs. Pitcher</th>
-            <th>Outcome</th>
+          <th className="header-cell">Inning & Count</th>
+          <th className="header-cell">Pitch Type & Speed</th>
+          <th className="header-cell">Batter vs. Pitcher</th>
+          <th className="header-cell">Outcome</th>
         </tr>
       </thead>
       <tbody>
-      {data.map((pitch, index) => (
+        {data.map((pitch, index) => (
           <tr key={index} onClick={() => onSelectPitch(pitch)}>
-            <td>{pitch.inning_half === '0' ? 'TOP' : 'BOTTOM'} {pitch.inning} ({pitch.strikes}-{pitch.balls})</td>
-            <td>{pitch.pitch_type} | {parseFloat(pitch.initial_speed).toFixed(0)} MPH </td>
-            <td>{pitch.batter_name} vs {pitch.pitcher_name}</td>
+            <td>
+              {pitch.inning_half === "0" ? "TOP" : "BOTTOM"} {pitch.inning} (
+              {pitch.strikes}-{pitch.balls})
+            </td>
+            <td>
+              {pitch.pitch_type} | {parseFloat(pitch.initial_speed).toFixed(0)}{" "}
+              MPH{" "}
+            </td>
+            <td>
+              {pitch.batter_name} vs {pitch.pitcher_name}
+            </td>
             <td>{pitch.event_type}</td>
           </tr>
         ))}
